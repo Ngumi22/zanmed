6cdbb8b96e9bd91eeac89340f4802754d384e5fc
Ngumi22 <mwaurapngumi@gmail.com>
2024-10-23 22:39:03 +0300

    Basic form setup for adding a product

---
+++ app/(backend)/dashboard/(products)/add-product/page.tsx
@@ -0,0 +1,10 @@
+import { CreateProductForm } from "@/components/dashboard/forms/addProductForm";
+import React from "react";
+
+export default function AddProduct() {
+  return (
+    <div>
+      <CreateProductForm />
+    </div>
+  );
+}
